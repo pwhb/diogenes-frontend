872cba1b9dcc0e760a64e833f4e9330b37141cd2
phonewaiyamhein <phonewaiyamhein@ayainnovation.com>
2023-02-15 16:30:13 +0630

    loading

--- src/routes/rooms/[id]/guess-the-number/+page.svelte
+++ src/routes/rooms/[id]/guess-the-number/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import Main from '$lib/components/games/guess-the-number/main.svelte';
+	import Main from '$lib/components/games/guess-the-number/guess-the-number.svelte';
 
 	import { socket } from '$lib/socketio/socket';
 	import { messagesStore } from '$lib/store/chat';
