1f5b6f1cadca24829236209cbe4edc68041a0e8f
phonewaiyamhein <phonewaiyamhein@ayainnovation.com>
2023-02-28 15:21:31 +0630

    update avatar

--- src/routes/api/users/[id]/+server.ts
+++ src/routes/api/users/[id]/+server.ts
@@ -1,5 +1,6 @@
 import dbConnect from '$lib/database/connectDB';
 import user from '$lib/models/user';
+import { getJwt } from '$lib/utils/jwt';
 import { getUpdateDocument } from '$lib/utils/validate';
 import { json, type RequestEvent, type RequestHandler } from '@sveltejs/kit';
 
@@ -18,18 +19,3 @@ export const GET: RequestHandler = async ({ params }: RequestEvent) => {
     }
 };
 
-export const PATCH: RequestHandler = async ({ params, request }: RequestEvent) => {
-    try {
-
-        const { id } = params;
-        const body = await request.json();
-        const keys = ["avatar"]
-        const update = getUpdateDocument(body, keys)
-        await dbConnect();
-        const updated = await user.findByIdAndUpdate(id, update, { new: true })
-        return json({ success: true, user: updated }, { status: 201 });
-    } catch (err) {
-        console.error(err);
-        return json({ success: false, error: err }, { status: 400 });
-    }
-}
