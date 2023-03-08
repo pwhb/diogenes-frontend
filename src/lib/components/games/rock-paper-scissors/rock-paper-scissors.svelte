8cb1bb9725cf609f5a3d69be0b0d370c9636dc29
phonewaiyamhein <phonewaiyamhein@ayainnovation.com>
2023-03-08 16:22:37 +0630

    rps

--- src/lib/components/games/rock-paper-scissors/rock-paper-scissors.svelte
+++ src/lib/components/games/rock-paper-scissors/rock-paper-scissors.svelte
@@ -4,6 +4,16 @@
 	import { onMount } from 'svelte';
 
 	console.log($gameState);
+	const availableChoices = {
+		rock: 'rock',
+		paper: 'paper',
+		scissors: 'scissors'
+	};
+	// @ts-ignore
+	const onTap = (choice) => {
+		console.log(choice);
+		console.log();
+	};
 
 	onMount(() => {
 		console.log('rps');
@@ -23,14 +33,14 @@
 	</div>
 
 	<div class="flex flex-row gap-10 items-center justify-center mb-20">
-		<button class="hover:scale-125 -rotate-12">
-			<Icon width={'54'} icon="noto:raised-fist-medium-light-skin-tone" />
+		<button class="hover:scale-125 -rotate-12" on:click={() => onTap(availableChoices.rock)}>
+			<Icon width={'54'} id="rock" icon="noto:raised-fist-medium-light-skin-tone" />
 		</button>
-		<button class="hover:scale-125">
+		<button class="hover:scale-125" on:click={() => onTap(availableChoices.paper)}>
 			<Icon width={'54'} icon="noto:hand-with-fingers-splayed-medium-light-skin-tone" />
 		</button>
-		<button>
-			<Icon class="hover:scale-125 rotate-12" width={'54'} icon="noto:victory-hand-medium-light-skin-tone" />
+		<button class="hover:scale-125 rotate-12" on:click={() => onTap(availableChoices.scissors)}>
+			<Icon width={'54'} icon="noto:victory-hand-medium-light-skin-tone" />
 		</button>
 	</div>
 </div>
